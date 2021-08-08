7138f4ebc9f537f50f66bb4a670ceb72fb3cce92
Anurag Shrivastava <anurag.dev443@gmail.com>
2021-08-08 14:01:06 +0530

    added ErrorBoundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,10 +2,9 @@ import React, { Fragment } from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  // if (true) throw new Error('Chhotu aya');
   const cardsArray = robots.map((robot, ind) => {
-    return (
-      <Card key={ind} id={robot.id} name={robot.name} email={robot.email} />
-    );
+    return <Card key={ind} id={robot.id} name={robot.name} email={robot.email} />;
   });
   return <Fragment>{cardsArray}</Fragment>;
 };
